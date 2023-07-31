786ba04af333e829a5a9c1346e6c316780d7fb08
dorothy416 <141013169+dorothy416@users.noreply.github.com>
2023-07-31 14:03:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_mnky2hr", "template_8wahgmf", formData)
+    emailjs.send("service_gwj500e", "mplate_lzk3ijp", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
